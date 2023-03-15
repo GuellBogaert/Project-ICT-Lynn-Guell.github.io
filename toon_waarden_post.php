b2db86039fae19337796bd90cc4e4da8c55f118a
GuellBogaert <122156205+GuellBogaert@users.noreply.github.com>
2023-03-15 23:25:09 +0100

    Add files via upload

---
+++ toon_waarden_post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Formulieren</title>
+		<link rel="stylesheet" href="Style.css">
+	</head>
+	
+	<body>
+		
+		
+		<?php
+
+        		
+		// afdrukken van de $_POST array
+		
+        print "<pre>" ;        
+        print_r ($_POST) ;        
+        print "</pre>" ;            
+        
+        ?>
+		
+	   	
+	</body>	
+	
+</html>
